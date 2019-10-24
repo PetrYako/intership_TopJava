50d70f3e20d154484bc620632dd0ae0c81c23831
Peotr <yakovenko.peotr@gmail.com>
2019-10-24 11:02:16 +0300

    1. Added table for meals. 2. Implemented JdbcMealRepository.

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
-CREATE SEQUENCE global_seq START WITH 100000;
+CREATE SEQUENCE global_seq START      WITH 100000;
+CREATE SEQUENCE global_meal_seq START WITH 100000;
 
 CREATE TABLE users
 (
@@ -21,5 +23,16 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_meal_seq'),
+  user_id     INTEGER    NOT NULL,
+  dateTime    TIMESTAMP  NOT NULL,
+  description VARCHAR    NOT NULL,
+  calories    INTEGER    NOT NULL,
+  CONSTRAINT meals_idx UNIQUE (user_id, id),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,8 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq      RESTART WITH 100000;
+ALTER SEQUENCE global_meal_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +11,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories) VALUES
+  (100000, timestamp '2019-10-23 9:00:00', 'Breakfast', 300),
+  (100000, timestamp '2019-10-23 12:00:00', 'Lunch', 500),
+  (100000, timestamp '2019-10-23, 19:00:00', 'Dinner', 500);

20afd0354ca19466dcc2e257c941c69ab6e300d7
Peotr <yakovenko.peotr@gmail.com>
2019-11-01 15:10:45 +0300

    1. Create Meal Hibernate entity 2. Implemented JpaMealRepository

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,41 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@NamedQueries({
+            @NamedQuery(name = Meal.BY_USER, query = "SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+            @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDate and m.dateTime <= :endDate ORDER BY m.dateTime DESC "),
+            @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.user.id =:userId and m.id =:id"),
+            @NamedQuery(name = Meal.FIND, query = "SELECT m FROM Meal m WHERE m.user.id=:userId and m.id=:id")
+        }
+)
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = "date_time", name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String GET_BETWEEN = "Meal.getBetween";
+    public static final String BY_USER = "Meal.getUserMeal";
+    public static final String DELETE = "Meal.delete";
+    public static final String FIND = "Meal.find";
+
+    @Column(name = "date_time", nullable = false, unique = true, columnDefinition = "default timestamp now()")
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "calories")
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Meal() {
